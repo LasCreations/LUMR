92a3270d529a0017214ae1744778003668398811
LasCreations <lascellemckenzie@gmail.com>
2023-12-16 14:13:47 -0500

    avater tick

--- app/client/public/static/scripts/dashboard.js
+++ app/client/public/static/scripts/dashboard.js
@@ -1,4 +1,16 @@
+function getSessionID() {
+    if (/\S/.test(document.cookie)) {
+        return document.cookie;
+    } else {
+        return null;
+    }
+}
+
 window.onload = function () {
+    fetchAndSetImage('/avatars/test.jpg', 'avatar1');
+    fetchAndSetImage('/avatars/dog.jpg', 'avatar2');
+    fetchAndSetImage('/avatars/cat.png', 'avatar3');
+
     //Authenticated user
     if (getSessionID() !== null) {
         fetch("/user/auth", {
@@ -7,16 +19,16 @@ window.onload = function () {
                 "Content-Type": "application/json",
             },
         }).then(res => {
-                if (res.ok) {
-                    return res.json(); // Parse the JSON from the response
-                } else {
-                    throw new Error('Network response was not ok');
-                }
-            })
+            if (res.ok) {
+                return res.json(); // Parse the JSON from the response
+            } else {
+                throw new Error('Network response was not ok');
+            }
+        })
             .then(data => {
                 // Handle the JSON data
                 console.log(data);
-                
+
                 // Access the elements where you want to display the specific fields
                 const emailContainer = document.getElementById('EmailContainer');
                 const passwordContainer = document.getElementById('PasswordContainer');
@@ -27,7 +39,7 @@ window.onload = function () {
                 passwordContainer.textContent = `Password: ${data.password}`;
                 usernameContainer.textContent = `Username: ${data.username}`;
 
-              })
+            })
             .catch(error => {
                 console.error("Error:", error.message);
             });
@@ -40,78 +52,37 @@ window.onload = function () {
     }
 };
 
-function getSessionID() {
-    if (/\S/.test(document.cookie)) {
-        return document.cookie;
-    } else {
-        return null;
-    }
+function fetchAndSetImage(url, elementId) {
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.blob(); // Convert the response to a Blob
+        })
+        .then(blob => {
+            // Create a data URL from the Blob
+            const imageUrl = URL.createObjectURL(blob);
+
+            // Set the image source
+            const imgElement = document.getElementById(elementId);
+            imgElement.src = imageUrl;
+
+            // Optional: Revoke the object URL to free up resources
+            // URL.revokeObjectURL(imageUrl);
+        })
+        .catch(error => console.error(`Error fetching image for ${elementId}:`, error));
 }
 
+function selectAvatar(avatarId) {
+    // Reset all ticks
+    const allTicks = document.querySelectorAll('.tick');
+    // allTicks.forEach(tick => tick.style.display = 'none');
 
+    // Show the tick for the selected avatar
+    // const selectedTick = document.getElementById(`tick${avatarId.charAt(avatarId.length - 1)}`);
+    // selectedTick.style.display = 'block';
 
-fetch('/avatars/test.jpg')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.blob(); // Convert the response to a Blob
-    })
-    .then(blob => {
-        // Create a data URL from the Blob
-        const imageUrl = URL.createObjectURL(blob);
-
-        // Set the image source
-        const imgElement = document.getElementById('avatar1');
-        imgElement.src = imageUrl;
-
-        // Optional: Revoke the object URL to free up resources
-        // URL.revokeObjectURL(imageUrl);
-    })
-    .catch(error => console.error('Error fetching image:', error));
-
-
-
-
-
-    fetch('/avatars/dog.jpg')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.blob(); // Convert the response to a Blob
-    })
-    .then(blob => {
-        // Create a data URL from the Blob
-        const imageUrl = URL.createObjectURL(blob);
-
-        // Set the image source
-        const imgElement = document.getElementById('avatar2');
-        imgElement.src = imageUrl;
-
-        // Optional: Revoke the object URL to free up resources
-        // URL.revokeObjectURL(imageUrl);
-    })
-    .catch(error => console.error('Error fetching image:', error));
-
-
-    fetch('/avatars/cat.png')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.blob(); // Convert the response to a Blob
-    })
-    .then(blob => {
-        // Create a data URL from the Blob
-        const imageUrl = URL.createObjectURL(blob);
-
-        // Set the image source
-        const imgElement = document.getElementById('avatar3');
-        imgElement.src = imageUrl;
-
-        // Optional: Revoke the object URL to free up resources
-        // URL.revokeObjectURL(imageUrl);
-    })
-    .catch(error => console.error('Error fetching image:', error));
-
+    // Store the selected avatar ID or perform any other necessary actions
+    console.log(`Selected Avatar: ${avatarId}`);
+}
