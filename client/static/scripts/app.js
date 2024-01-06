5d434832610be8d7f17e2cf81de536b166bfffa6
LasCreations <lascellemckenzie@gmail.com>
2024-01-06 05:16:19 -0500

    added galaxy theme

--- client/static/scripts/app.js
+++ client/static/scripts/app.js
@@ -23,10 +23,28 @@ document.addEventListener('DOMContentLoaded', function () {
     }
     
     
-    // else if (window.location.href.includes('/pages/dashboard.html')) {
-    //     var data = JSON.parse(atob(decodeURIComponent(window.location.search.replace('?data=', ''))));
-    //     console.log(data);  // Outputs: data from signup
-    // }
+    const starField = document.querySelector(".stars");
+
+    // Function to generate random number within a range
+    function randomRange(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    // Function to create a star and append it to the starField
+    function createStar() {
+        const star = document.createElement("div");
+        star.className = "star";
+        star.style.left = `${randomRange(0, 120)}vw`;
+        star.style.top = `${randomRange(0, 150)}vh`;
+        star.style.animationDuration = `${randomRange(2, 6)}s`;
+        starField.appendChild(star);
+    }
+
+    // Create a certain number of stars
+    const numberOfStars = 700;
+    for (let i = 0; i < numberOfStars; i++) {
+        createStar();
+    }
 });
 
 
